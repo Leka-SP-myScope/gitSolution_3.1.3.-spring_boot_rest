109977ab1577ac63307c2a9036a4dfa42dd1c0c6
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-09-03 14:06:36 +0300

    Commit 2 03.09.21 Changed 2 files.

--- src/main/resources/static/user_pageForUpdated.js
+++ src/main/resources/static/user_pageForUpdated.js
@@ -2,26 +2,54 @@
 
 //test1.insertAdjacentText("About new USER");
 
-fetch("http://localhost:8080/api/user")
-.then(response => {
-    if (response.ok) {
-        response.json().then(data => {
-            if (data.length > 0) {
-                console.log(data);
-                let temp = "";
-                data.forEach((x) => {
-                    temp += "<tr>";
-                    temp += "<td class=\"border-left-0\">" + x.id + "</td>";
-                    temp += "<td>" + x.name + "</td>";
-                    temp += "<td>" + x.surname + "</td>";
-                    temp += "<td>" + x.age + "</td>";
-                    temp += "<td>" + x.email + "</td>";
-                    temp += "<td class=\"border-left-0\">" + x.id + "</td></tr>";
-                });
-                document.getElementById("data").innerText = temp;
-            } else {
-                console.log('Request failed with response ' + response.status + ': ' + response.statusText);
-            }
-        })
+async function loadUsersInTable(url, table) {
+    const tableBody = table.querySelector('#bodyUsers');
+    const response = await fetch(url);
+    const data = await response.json();
+
+//    Clear the table
+    tableBody.innerHTML = "";
+
+//    Populate the bodies
+    for(const row of data) {
+        const rowElement = document.createElement('<tr>');
+        for (const cellText of row) {
+            const cellElement = document.createElement('<td>');
+            cellElement.textContent = cellText;
+            rowElement.appendChild(cellElement);
+        }
+        tableBody.appendChild(rowElement);
     }
-});
+
+    // data.forEach((x) => {
+    //     const rowElement = document.createElement('<tr>');
+    //
+    //
+    // })
+}
+
+// fetch("http://localhost:8080/api/user")
+// .then(response => {
+//     if (response.ok) {
+//         response.json().then(data => {
+//             if (data.length > 0) {
+//                 console.log(data);
+//                 let temp = "";
+//                 data.forEach((x) => {
+//                     temp += "<tr>";
+//                     temp += "<td class=\"border-left-0\">" + x.id + "</td>";
+//                     temp += "<td>" + x.name + "</td>";
+//                     temp += "<td>" + x.surname + "</td>";
+//                     temp += "<td>" + x.age + "</td>";
+//                     temp += "<td>" + x.email + "</td>";
+//                     temp += "<td class=\"border-left-0\">" + x.id + "</td></tr>";
+//                 });
+//                 document.getElementById("bodyUsers").innerText = temp;
+//             } else {
+//                 console.log('Request failed with response ' + response.status + ': ' + response.statusText);
+//             }
+//         })
+//     }
+// });
+
+loadUsersInTable("http://localhost:8080/api/user", document.querySelector('#tableUsers'));
