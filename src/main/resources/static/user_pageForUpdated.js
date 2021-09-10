a220c4b27352b51783e62a770f0fb36d4a66bf5b
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-09-10 12:46:42 +0300

    Commit 2 10.09.21 Changed 2 files, populate: the table, the headTable.

--- src/main/resources/static/user_pageForUpdated.js
+++ src/main/resources/static/user_pageForUpdated.js
@@ -6,34 +6,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function loadUsersInTable(url, table) {
     const tableBody = table.querySelector('#bodyUsers');
+    const tableHead = table.querySelector('#headUsers');
     const response = await fetch(url);
     const data = await response.json();
+
+    //Create tableHead
+    let head = "";
+    head += "<tr>";
+    head += "<th class='border-left-0'>ID</th>";
+    head += "<th>First Name</th>";
+    head += "<th>LastName</th>";
+    head += "<th>Age</th>";
+    head += "<th>Email</th>";
+    head += "<th class='border-right-0'>Role</th>";
+    head += "</tr>";
+
+    document.getElementById("headUsers").innerHTML = head;
 
 //    Clear the table
     tableBody.innerHTML = "";
 
 //    Populate the body of table
-    let temp = "";
+    let row = "";
     data.forEach((user) => {
-        temp += "<tr>";
-        temp += "<td class=\"border-left-0\">" + user.id + "</td>";
-        temp += "<td>" + user.name + "</td>";
-        temp += "<td>" + user.surname + "</td>";
-        temp += "<td>" + user.age + "</td>";
-        temp += "<td>" + user.email + "</td>";
-        temp += "<td class=\"border-left-0\">" + user.id + "</td></tr>";
+        row += "<tr>";
+        row += "<td class='border-left-0'>" + user.id + "</td>";
+        row += "<td>" + user.name + "</td>";
+        row += "<td>" + user.surname + "</td>";
+        row += "<td>" + user.age + "</td>";
+        row += "<td>" + user.email + "</td>";
+        row += "<td class='border-right-0'>" + user.id + "</td></tr>";
     });
-
-    data.forEach((user) => {
-        temp += "<tr>";
-        temp += "<td class=\"border-left-0\">" + user.id + "</td>";
-        temp += "<td>" + user.name + "</td>";
-        temp += "<td>" + user.surname + "</td>";
-        temp += "<td>" + user.age + "</td>";
-        temp += "<td>" + user.email + "</td>";
-        temp += "<td class=\"border-left-0\">" + user.id + "</td></tr>";
-    });
-    document.getElementById('bodyUsers').innerHTML = temp;
-}
+    document.getElementById('bodyUsers').innerHTML = row;
+    }
 
 loadUsersInTable("http://localhost:8080/api/user", document.querySelector('#tableUsers'));
