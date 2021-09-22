b2cb505d58810589a9e1676e1c3b2ecc3c6162c5
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-09-22 20:08:19 +0300

    Commit 1 22.09.21 Changed 1 files, working application populate table.

--- src/main/resources/static/user_pageForUpdated.js
+++ src/main/resources/static/user_pageForUpdated.js
@@ -31,24 +31,44 @@ async function loadUsersInTable(url, table) {
 //    Populate the body of table
         let row = "";
 
-
         console.log(data);
-        for (let user of data) {
-            console.log(user.role);
+        console.log(data[2].roles[0].role);
+        console.log(data[2].roles[0].role);
+
+        // for (i in data) {
+        //     role = data.roles[i];
+        //     console.log(role);
+        // }
+
+        for (let i = 0; i < data.length; i++) {
+            //console.log(data.roles.role);
             row += "<tr>";
-            row += "<td class='border-left-0'>" + user.id + "</td>";
-            row += "<td>" + user.name + "</td>";
-            row += "<td>" + user.surname + "</td>";
-            row += "<td>" + user.age + "</td>";
-            row += "<td>" + user.email + "</td>";
-            row += "<td class='border-right-0'>" + user + "</td></tr>";
-
-
-            //console.log(user[roles][1]);
-            //console.log(user[1]);
-            //console.log(user[0].rolesNameList);
-            //console.log(user.rolesNameList.role);
+            row += "<td class='border-left-0'>" + data[i].id + "</td>";
+            row += "<td>" + data[i].name + "</td>";
+            row += "<td>" + data[i].surname + "</td>";
+            row += "<td>" + data[i].age + "</td>";
+            row += "<td>" + data[i].email + "</td>";
+            row += "<td class='border-right-0'>" + data[i].roles[0].role + "</td></tr>";
         }
+
+        // for (let user of data) {
+        //     //console.log(data.roles.role);
+        //     row += "<tr>";
+        //     row += "<td class='border-left-0'>" + user.id + "</td>";
+        //     row += "<td>" + user.name + "</td>";
+        //     row += "<td>" + user.surname + "</td>";
+        //     row += "<td>" + user.age + "</td>";
+        //     row += "<td>" + user.email + "</td>";
+        //     row += "<td class='border-right-0'>" + user + "</td></tr>";
+        //
+        //
+        //
+        //
+        //     //console.log(user[roles][1]);
+        //     //console.log(user[1]);
+        //     //console.log(user[0].rolesNameList);
+        //     //console.log(user.rolesNameList.role);
+        // }
         //console.log(data);
         // data.forEach((user) => {
         //     console.log(user);
