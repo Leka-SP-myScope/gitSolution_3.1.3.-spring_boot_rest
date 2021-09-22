c94c3e4bf12b03ffd29001f26c033f717dc5fdff
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-09-22 21:51:11 +0300

    Commit 3 22.09.21 Changed 1 files, working application without header.

--- src/main/resources/static/user_pageForUpdated.js
+++ src/main/resources/static/user_pageForUpdated.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
 async function loadUsersInTable(url, table) {
     const tableBody = table.querySelector('#bodyUsers');
     const tableHead = table.querySelector('#headUsers');
-    
+
     try {
         const response = await fetch(url);
         const data = await response.json();
@@ -25,50 +25,11 @@ async function loadUsersInTable(url, table) {
 
         document.getElementById("headUsers").innerHTML = head;
 
-//    Clear the table
+        //Clear the table
         tableBody.innerHTML = "";
 
-//    Populate the body of table
+        //Populate the body of table
         let row = "";
-
-        console.log(data);
-        console.log(data[2].roles[0].role);
-        console.log(data[2].roles[0].role);
-
-        //Working loop-1
-        // for (let i = 0; i < data.length; i++) {
-        //     row += "<tr>";
-        //     row += "<td class='border-left-0'>" + data[i].id + "</td>";
-        //     row += "<td>" + data[i].name + "</td>";
-        //     row += "<td>" + data[i].surname + "</td>";
-        //     row += "<td>" + data[i].age + "</td>";
-        //     row += "<td>" + data[i].email + "</td>";
-        //     row += "<td class='border-right-0'>" + data[i].roles[0].role.substring(5) + "</td></tr>";
-        // }
-
-        //Working loop-2
-        // for (let user of data) {
-        //     row += "<tr>";
-        //     row += "<td class='border-left-0'>" + user.id + "</td>";
-        //     row += "<td>" + user.name + "</td>";
-        //     row += "<td>" + user.surname + "</td>";
-        //     row += "<td>" + user.age + "</td>";
-        //     row += "<td>" + user.email + "</td>";
-        //     row += "<td class='border-right-0'>" + user.roles[0].role.substring(5) + "</td></tr>";
-        // }
-
-        //Working loop-3
-        // data.forEach((user) => {
-        //     row += "<tr>";
-        //     row += "<td class='border-left-0'>" + user.id + "</td>";
-        //     row += "<td>" + user.name + "</td>";
-        //     row += "<td>" + user.surname + "</td>";
-        //     row += "<td>" + user.age + "</td>";
-        //     row += "<td>" + user.email + "</td>";
-        //     row += "<td class='border-right-0'>" + user.roles[0].role.substring(5) + "</td></tr>";
-        // });
-
-        //Working loop-4
         data.forEach((user) => {
             row += "<tr>";
             row += "<td class='border-left-0'>" + user.id + "</td>";
