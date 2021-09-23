ee0b756f4512dd13ae7a95a13e1f2100b6f82f78
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-09-23 23:03:22 +0300

    Commit 2 23.09.21 Changed 2 files, working application with header.

--- src/main/resources/static/header.js
+++ src/main/resources/static/header.js
@@ -1,38 +1,25 @@
-// alert("It's Header");
+async function loadHeader(url, header) {
+    try {
+        const response = await fetch(url);
+        const data = response.json();
 
-let header = document.getElementById("header");
-    let head = "";
-    head += "<nav class='navbar navbar-light navbar-expand-md navigation-clean'>";
-    head += "<div class='container-fluid'>";
-    head += "<div class='container my-nav-container1'>";
-    head += "<b><label class='text-lowercase text-center text-light' id='email-nav' th:text='${#authentication.getPrincipal().getEmail()}'>admin@mail.ru</label></b>";
-    head += "<label class='text-center' id='text'>with roles:</label>";
-    head += "<label class='text-uppercase text-left' id='role-nav' text='${#authentication.getPrincipal().getUsername()}'>admin</label></div>";
-    head += "<button data-toggle='collapse' class='navbar-toggler' data-target=''#navcol-1'><span class='sr-only'>Toggle navigation</span><span class='navbar-toggler-icon'></span></button>";
-    head += "<div class='collapse navbar-collapse' id='navcol-1'>";
-    head += "<ul class='navbar-nav ml-auto'>";
-    head += "<li class='nav-item'><a class='nav-link text-center text-secondary my-nav-link-logout' href='/login'>Logout</a></li></ul></div></div></nav></div>";
+        let head = "";
+        head += "<nav class='navbar navbar-light navbar-expand-md navigation-clean'>";
+        head += "<div class='container-fluid'>";
+        head += "<div class='container my-nav-container1'>";
+        head += "<b><label class='text-lowercase text-center text-light' id='email-nav' th:text='${#authentication.getPrincipal().getEmail()}'>admin@mail.ru</label></b>";
+        head += "<label class='text-center' id='text'>with roles:</label>";
+        head += "<label class='text-uppercase text-left' id='role-nav' text='${#authentication.getPrincipal().getUsername()}'>admin</label></div>";
+        head += "<button data-toggle='collapse' class='navbar-toggler' data-target=''#navcol-1'><span class='sr-only'>Toggle navigation</span><span class='navbar-toggler-icon'></span></button>";
+        head += "<div class='collapse navbar-collapse' id='navcol-1'>";
+        head += "<ul class='navbar-nav ml-auto'>";
+        head += "<li class='nav-item'><a class='nav-link text-center text-secondary my-nav-link-logout' href='/login'>Logout</a></li></ul></div></div></nav></div>";
+        header.innerHTML = head + "This is a header!";
 
+    } catch (e) {
+        console.log(e);
+    }
+}
 
+loadHeader("http://localhost:8080/api/user", document.querySelector('#header'));
 
-
-header.innerHTML = head + "This is a header!";
-
-
-// <div th:fragment="header" th:remove="tag">
-//     <nav class="navbar navbar-light navbar-expand-md navigation-clean">
-//     <div class="container-fluid">
-//          <div class="container my-nav-container1">
-//                  <b><label class="text-lowercase text-center text-light" id="email-nav" for="email" th:text="${#authentication.getPrincipal().getEmail()}">user@mail.ru</label></b>
-//                 <label class="text-center" id="text">with roles:</label>
-//                  <label class="text-uppercase text-left" id="role-nav" for="role" th:text="${#authentication.getPrincipal().getUsername()}">user</label>
-//          </div>
-//          <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-//              <div class="collapse navbar-collapse" id="navcol-1">
-//                  <ul class="navbar-nav ml-auto">
-//                      <li class="nav-item"><a class="nav-link text-center text-secondary my-nav-link-logout" href="/login">Logout</a></li>
-//                  </ul>
-//              </div>
-//          </div>
-//      </nav>
-// </div>
