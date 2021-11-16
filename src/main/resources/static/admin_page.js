e5da24aee5b03401fc6acd72bc6763a650c54326
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-11-16 15:16:20 +0300

    Commit 1 16.11.21 changed 1 file.

--- src/main/resources/static/admin_page.js
+++ src/main/resources/static/admin_page.js
@@ -45,8 +45,6 @@ async function loadUsersInTableAllUsers(url, table) {
         });
         document.getElementById('bodyAllUsers').innerHTML = row;
 
-        tableBodyNewUser
-
         //Populate the body of tableNewUser
         let rowNewUser = "";
         rowNewUser += "<tr>";
@@ -54,9 +52,6 @@ async function loadUsersInTableAllUsers(url, table) {
         rowNewUser += "<div class='col-4 offset-4'>";
         rowNewUser += "<div class='row'>";
         rowNewUser += "<div class='col'>";
-
-
-
         rowNewUser += "<form class='text-center' action='/admin/users' method='post'>" +
             "<div class='form-group'><label id='labelFirstName' for='name'><strong>First name</strong></label><input class='form-control' type='text' id='name' name='name' placeholder='First name' required=''></div>" +
             "<div class='form-group'><label id='labelLastName' for='surname'><strong>Last name</strong></label><input class='form-control' type='text' id='surname' name='surname' placeholder='Last name' required=''></div>" +
@@ -64,30 +59,13 @@ async function loadUsersInTableAllUsers(url, table) {
             "<div class='form-group'><label id='labelEmail' for='email'><strong>Email</strong></label><input class='form-control' type='email' id='email' placeholder='Email' name='email' required=''></div>" +
             "<div class='form-group'><label id='password' for='password'><strong>Password</strong></label><input class='form-control' type='password' id='password1' name='password' placeholder='Password' required=''></div>" +
             "<div class='form-group'><label id='role' for='role'><strong>Role</strong></label><select class='form-control' multiple='multiple' name='rolesNameList'>" +
-
             "<option>ADMIN</option> " +
             "<option>USER" +
-
-
-            // "<option th:each='role : ${listRoles}'\n" +
-            // "th:value='${!role.getRole().startsWith('ROLE_')}? ${role.getRole()}: ${role.getRole().replaceFirst('ROLE_', '')}'\n" +
-            // "th:text='${!role.getRole().startsWith('ROLE_')}? ${role.getRole()}: ${role.getRole().replaceFirst('ROLE_', '')}'>\n" +
             "</option></select></div>" +
             "<button class='btn btn-success' type='submit'>Add new user</button>" +
             "</form>";
-
-
-
-
-
-
-            //rowNewUser += "<div class='col-4 offset-4'>";
-            rowNewUser += "</div></div></div></td></tr>";
-
-
-
+        rowNewUser += "</div></div></div></td></tr>";
         document.getElementById('bodyNewUser').innerHTML = rowNewUser;
-
     } catch (e) {
         console.log(e);
     }
@@ -137,6 +115,47 @@ async function loadUsersInTable(url, table) {
         console.log(e);
     }
 }
+const buttonDelete = document.querySelector('#btnDeleteModal');
+function deleteUsersInTable(url, table) {
+    //const tableBody = table.querySelector('#bodyAllUsers');
+
+    //const buttonDelete = document.querySelector('#btnDeleteModal');
+    //const url1 = "http://localhost:8080/api/users/";
+
+    function deleteUser() {
+        fetch(url, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(() => location.reload());
+    }
+    buttonDelete.addEventListener('click', deleteUser);
+
+    // try {
+    //     // const response = await fetch(url);
+    //     // const data = await response.json();
+    //
+    //     //Create button DELETE
+    //
+    //     const buttonDelete = document.querySelector('#btnDeleteModal');
+    //     //const url1 = "http://localhost:8080/api/users/";
+    //
+    //     function deleteUser() {
+    //         fetch(url, {
+    //             method: 'DELETE',
+    //         })
+    //             .then(res => res.json())
+    //             .then(() => location.reload());
+    //     }
+    //     buttonDelete.addEventListener('click', deleteUser);
+    // } catch (e) {
+    //     console.log(e);
+    // }
+}
+
+
+
 
 loadUsersInTable("http://localhost:8080/api/users", document.querySelector('#tableAboutUser'));
 loadUsersInTableAllUsers("http://localhost:8080/api/users", document.querySelector('#tableAllUsers'));
+deleteUsersInTable("http://localhost:8080/api/users/25", document.querySelector('#tableAllUsers'));
